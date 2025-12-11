09ee55e286c489b9c6b49e737c2e1460876d1316
Faiz Rifiyanto <93854420+justpaisss@users.noreply.github.com>
2025-12-11 16:56:13 +0700

    menambahkan modul 8

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@
 URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# mysite/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # Pastikan include di-import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')), # Tambahkan baris ini
 ]
+
